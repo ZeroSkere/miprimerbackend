d1b29d5889aad8bd3048fedb9b6928976bc3e872
carlos daniel herrera chavez <carlosdhc03@gmail.com>
2022-11-27 10:27:54 -0400

    prueba

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express from 'npm:express'
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+
+    res.send('Q fue rey')
+}
+)
+
+app.listen(3000, () => {
+
+    console.log('Q fue');
+
+} )
